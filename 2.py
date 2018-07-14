a522f675819869190d8d7838efe4952793aa1ca9
Aswin R Prasad <aswinrprasad@gmail.com>
2018-07-15 03:38:56 +0530

    Simple Codes

---
+++ 2.py
@@ -0,0 +1,6 @@
+a=raw_input("Enter the first name :")
+b=raw_input("Enter the second name :")
+
+c=a+b
+
+print c
---
+++ 3.py
@@ -0,0 +1,21 @@
+#Converting string to float and integer
+
+a="1"
+b="2"
+
+c=a+b
+print c
+
+ai=int(a)
+bi=int(b)
+ci=ai/bi
+di=ai*bi
+
+print ci,"and",di
+
+af=float(a)
+bf=float(b)
+cf=af/bf
+df=af*bf
+
+print cf,"and",df
