8de1459942a0f303b3bb2495ef2fdf63b523fca5
Aswin R Prasad <aswinrprasad@gmail.com>
2018-07-28 21:11:16 +0530

    Question Update

--- Strings and Numbers/4.py
+++ Strings and Numbers/4.py
@@ -1,3 +1,5 @@
+#You are writing a program that process the details of new students entering a high school. The user inputs, the name, their date of birth, the last 4 digits of their personal number. The program outputs: the name and personal number as a single string, and also the age of the student. Depending on how you have wrote the program you might need to use str(number) In this context can you work what str(number) does?
+
 name=raw_input("Enter the name of the student :")
 date = raw_input("Enter the date in DOB : ")
 month = raw_input("Enter the month in DOB : ")
