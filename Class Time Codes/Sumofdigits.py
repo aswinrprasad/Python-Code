c561fd26e6e2914cb43cec1ecad31388ec4a1c07
Aswin R Prasad <aswinrprasad@gmail.com>
2018-08-10 00:23:02 +0530

    Assert Function

--- Class Time Codes/Sumofdigits.py
+++ Class Time Codes/Sumofdigits.py
@@ -3,11 +3,12 @@
 t=input("Enter no.of test  cases :")
 li=[]
 
-if t>=1 and t<=30 :
-	print "Enter values :"
-	for i in range(0,t) :
-		obj=int(input())
-	        li.append(obj)
+assert(t>=1 and t<=30)
+
+print "Enter values :"
+for i in range(0,t) :
+	obj=int(input())
+        li.append(obj)
 
 for i in range(0,t):
 	sum1=0
@@ -17,4 +18,4 @@ for i in range(0,t):
         	sum1+=int(val)
         	li[i]/=10
     	print "The sum of",dup,"is :",sum1
-    
+
