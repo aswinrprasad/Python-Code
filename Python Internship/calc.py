69f433481391ba4a25a6cbb6de4c40face581c76
aswinrprasad <aswinrprasad@gmail.com>
2018-12-28 16:03:36 +0530

    Module:Calc functions available

---
+++ Python Internship/calc.py
@@ -0,0 +1,17 @@
+def add2(a,b):
+	return a+b
+
+def sub2(a,b):
+	return a-b
+
+def div2(a,b):
+	return a/b
+
+def mul2(a,b):
+	return a*b
+
+def rem1(a,b):
+	return a%b
+
+def pow(a,b):
+	return a**b
