7cc75d261d5f9a305d7acc5ef3ebc825f014fb9f
aswinrprasad <aswinrprasad@gmail.com>
2018-12-29 16:23:25 +0530

    Buggy trig functions

--- Python Internship/calc.py
+++ Python Internship/calc.py
@@ -20,17 +20,29 @@ def pow(a,b):
 
 def sinc(a,b):
 	return a*m.sin(m.radians(b))
+
 def cosc(a,b):
 	return a*m.cos(m.radians(b))
 
 def tanc(a,b):
-	return a*m.tan(m.radians(b))
+	if m.cos(m.radians(b)) != 0: 
+		return a*(m.tan(m.radians(b)))
+	else:
+		return "Not Defined!"
 
 def cosecc(a,b):
-	return a*(1/m.sin(m.radians(b)))
-
+	if m.sin(m.radians(b)) != 0: 
+		return a*(1/m.sin(m.radians(b)))
+	else:
+		return "Not Defined!"	
 def secc(a,b):
-	return a*(1/m.cos(m.radians(b)))
+	if m.cos(m.radians(b)) != 0: 
+		return a*(1/m.cos(m.radians(b)))
+	elif m.cos(m.radians(b)) == float("inf"):
+		return "Not Defined!"
 
 def cotc(a,b):
-	return a*(1/m.tan(m.radians(b)))
+	if m.tan(m.radians(b)) != 0: 
+		return a*(1/m.tan(m.radians(b)))
+	else:
+		return "Not Defined!"
