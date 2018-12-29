f56101607c899da9d6cf9d4f160c0d9071054c60
aswinrprasad <aswinrprasad@gmail.com>
2018-12-30 01:37:28 +0530

    Exp and Log function

--- Python Internship/calc.py
+++ Python Internship/calc.py
@@ -19,14 +19,22 @@ def pow(a,b):
 	return a**b
 
 def sinc(a,b):
-	return a*m.sin(m.radians(b))
+	n=(b-0)%180
+	if n==0:
+		return 0
+	else:
+		return a*m.sin(m.radians(b))
 
 def cosc(a,b):
-	return a*m.cos(m.radians(b))
+	n=(b-90)%180
+	if n==0:
+		return 0
+	else:
+		return a*m.cos(m.radians(b))
 
 def tanc(a,b):
-	if m.cos(m.radians(b)) != 0: 
-		return a*(m.tan(m.radians(b)))
+	if cosc(1,b) != 0: 
+		return a*(sinc(a,b)/cosc(a,b))
 	else:
 		return "Not Defined!"
 
@@ -36,13 +44,22 @@ def cosecc(a,b):
 	else:
 		return "Not Defined!"	
 def secc(a,b):
-	if m.cos(m.radians(b)) != 0: 
+	if cosc(1,b) != 0: 
 		return a*(1/m.cos(m.radians(b)))
-	elif m.cos(m.radians(b)) == float("inf"):
+	else:
 		return "Not Defined!"
 
 def cotc(a,b):
-	if m.tan(m.radians(b)) != 0: 
-		return a*(1/m.tan(m.radians(b)))
+	if sinc(1,b) != 0: 
+		return a*(cosc(a,b)/sinc(a,b))
 	else:
 		return "Not Defined!"
+
+def logc(a,b):
+	if b <= 0:
+		return "Not defined!"
+	else:
+		return a*m.log10(b)
+
+def expc(a,b):
+		return a*m.exp(b)
