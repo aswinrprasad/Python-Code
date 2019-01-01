f793e7e7943cc01fee0a5885da2370284d99bfe1
aswinrprasad <aswinrprasad@gmail.com>
2019-01-02 01:23:18 +0530

    PyCal v0.7 : Ability to store previous result

--- PROJECT PyCal/Calculator.py
+++ PROJECT PyCal/Calculator.py
@@ -5,20 +5,39 @@ def word(cal,j):
 	while cal[j]>='a' and cal[j] <= 'z' or cal[j]>='A' and cal[j] <= 'Z':
 		op1+=cal[j]
 		j+=1
-	if op1=='Sin' or op1=='sin' or op1=='Cos' or op1=='cos' or op1=='Tan' or op1=='tan' or op1=='cosec' or op1=='Cosec' or op1=='Sec' or op1=='sec' or op1=='Cot' or op1=='cot' or op1=='log' or op1 == 'e' or op1 == 'p' or op1== 'c':
+	if op1=='Ans' or op1=='Sin' or op1=='sin' or op1=='Cos' or op1=='cos' or op1=='Tan' or op1=='tan' or op1=='cosec' or op1=='Cosec' or op1=='Sec' or op1=='sec' or op1=='Cot' or op1=='cot' or op1=='log' or op1 == 'e' or op1 == 'p' or op1== 'c':
 		return op1
 	else :
 		print "Invalid!! Try again!!"
 		exit(0)
 
+def Answer(n1,ch,opa):
+	i=0
+	while i<len(ch):
+		if ch[i]>='0' and ch[i]<='9' or ch[i]=='.' or ch[i]== '-' or ch[i] >='a' and ch[i] <= 'z' or ch[i]>='A' and ch[i] <= 'Z':
+			k=0
+		else:
+			opa=i
+			if ch[opa] == ' ':	
+				print "Invalid operand! Try again!"
+				exit(0)
+			break
+		i+=1
+	n2=''
+	while(i<len(ch)):
+		if ch[i]>='0' and ch[i]<='9' or ch[i]=='.' or ch[i]== '-':
+			n2+=ch[i]
+		i+=1
+	return opa,str(n2)
 
-print """\n\n\t\t******** PYCAL v0.6\t
+
+print """\n\n\t\t******** PYCAL v0.7\t
 		\t-Made By ASWIN RAGHUPRASAD ********\t\n\n"""
 print """\tNOTE:It is a binary calculator which accepts two inputs and an operator.\t\n\n"""
 
 
 while(True):
-	print "MENU of available calculations :\n1.Add(+)\n2.Subtract(-)\n3.Divivde(/)\n4.Multiply(*)\n5.Remainder(%)\n6.Power(^)\n7.Trigonometric functions(eg:aSin(b))\n8.Log(eg:alogb)\n9.Exponential(eg:ae(b))\n10.Factorial(eg:5!)\n11.Permutation(eg:4p2)\n12.Combinations(eg:4c2)\nType Exit/exit to quit the program :\n\n",
+	print "MENU of available calculations :\n1.Add(+)\n2.Subtract(-)\n3.Divivde(/)\n4.Multiply(*)\n5.Remainder(%)\n6.Power(^)\n7.Trigonometric functions(eg:aSin(b))\n8.Log(eg:alogb)\n9.Exponential(eg:ae(b))\n10.Factorial(eg:5!)\n11.Permutation(eg:4p2)\n12.Combinations(eg:4c2)\nPs:Previous results can be used.(eg:Ans+3)\nType Exit/exit to quit the program :\n\n",
 
 	ch=raw_input("Enter a calculation(eg: 4+3) to be done : ")
 	if ch == 'Exit' or ch== 'exit':
@@ -54,44 +73,54 @@ while(True):
 		i+=1
 
 
+	if trigop =='Ans':
+		try:
+			opa=0
+			n1=str(Ans)
+			opf,n2=Answer(n1,ch,opa)
+		except NameError:
+			print "You have not done any calculations yet for previous result to be stored! Try again!\n\n"
+			continue
+
 	if ch[opf]=='+' :
-		print "\n",n1+ch[opf]+n2,"=",
-		print op.add2(float(n1),float(n2))
+		Ans=op.add2(float(n1),float(n2))
+		print "\n",n1+ch[opf]+n2,"=",Ans
 	elif ch[opf]=='-' :
-		print "\n",n1+ch[opf]+n2,"=",
-		print op.sub2(float(n1),float(n2))
+		Ans=op.sub2(float(n1),float(n2))
+		print "\n",n1+ch[opf]+n2,"=",Ans
 	elif ch[opf]=='/' :
-		print "\n",n1+ch[opf]+n2,"=",
-		print op.div2(float(n1),float(n2))
+		Ans=op.div2(float(n1),float(n2))
+		print "\n",n1+ch[opf]+n2,"=",Ans
 	elif ch[opf]=='*' :	
-		print "\n",n1+ch[opf]+n2,"=",
-		print op.mul2(float(n1),float(n2))
+		Ans=op.mul2(float(n1),float(n2))
+		print "\n",n1+ch[opf]+n2,"=",Ans
 	elif ch[opf]=='%' :
-		print "\n",n1+ch[opf]+n2,"=",
-		print op.rem1(float(n1),float(n2))
+		Ans=op.rem1(float(n1),float(n2))
+		print "\n",n1+ch[opf]+n2,"=",Ans
 	elif ch[opf]=='^' :	
-		print "\n",n1+ch[opf]+n2,"=",
-		print op.pow(float(n1),float(n2))
+		Ans=op.pow(float(n1),float(n2))
+		print "\n",n1+ch[opf]+n2,"=",Ans
 	elif trigop == 'Sin' or trigop == 'sin':
-		print "\n",n1+trigop+n2,"=",
-		print op.sinc(float(n1),float(n2))
+		Ans=op.sinc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,"=",Ans
 	elif trigop == 'Cos' or trigop == 'cos':
-		print "\n",n1+trigop+n2,"=",
-		print op.cosc(float(n1),float(n2))
+		Ans=op.cosc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,"=",Ans
 	elif trigop == 'Tan' or trigop == 'tan':
-		print "\n",n1+trigop+n2,"=",
-		print op.tanc(float(n1),float(n2))
+		Ans=op.tanc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,"=",Ans
 	elif trigop == 'Cosec' or trigop == 'cosec':
-		print "\n",n1+trigop+n2,"=",
-		print op.cosecc(float(n1),float(n2))
+		Ans=op.cosecc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,"=",Ans
 	elif trigop == 'Sec' or trigop == 'sec':
-		print "\n",n1+trigop+n2,"=",
-		print op.secc(float(n1),float(n2))
+		Ans=op.secc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,"=",Ans
 	elif trigop == 'Cot' or trigop == 'cot':
-		print "\n",n1+trigop+n2,"=",
-		print op.cotc(float(n1),float(n2))
+		Ans=op.cotc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,"=",Ans
 	elif trigop == 'log':
-		print "\n",n1+trigop+n2,'=',op.logc(float(n1),float(n2))
+		Ans=op.logc(float(n1),float(n2))
+		print "\n",n1+trigop+n2,'=',Ans
 	elif trigop == 'e':
 		print "\n",n1+trigop+"("+n2+")",'=',op.expc(float(n1),float(n2))
 	elif ch[opf] == '!':
@@ -99,7 +128,8 @@ while(True):
 			if op.fact(int(n1)) == None:
 				print "Cannot find factorial of a negative number.Try again!\n\n"
 				continue
-			print "\n",n1+ch[opf],"=",op.fact(int(n1))
+			Ans=op.fact(int(n1))
+			print "\n",n1+ch[opf],"=",Ans
 		except ValueError:
 			print "Input cannot be a decimal value for factorial calculation!! Try again!"
 	elif trigop == 'p':
@@ -107,7 +137,8 @@ while(True):
 			if op.fact(int(n1)) == None or op.fact(int(n2)) == None:
 				print "Cannot find factorial of a negative number.Try again!\n\n"
 				continue
-			print "\n",n1+trigop+n2,"=",op.perm(int(n1),int(n2))
+			Ans=op.perm(int(n1),int(n2))
+			print "\n",n1+trigop+n2,"=",Ans
 		except ValueError:
 			print "Input cannot be a decimal value for factorial calculation!! Try again!"
 	elif trigop == 'c':
@@ -115,7 +146,8 @@ while(True):
 			if op.fact(int(n1)) == None or op.fact(int(n2)) == None:
 				print "Cannot find factorial of a negative number.Try again!\n\n"
 				continue
-			print "\n",n1+trigop+n2,"=",op.comb(int(n1),int(n2))
+			Ans=op.comb(int(n1),int(n2))
+			print "\n",n1+trigop+n2,"=",Ans
 		except ValueError:
 			print "Input cannot be a decimal value for factorial calculation!! Try again!"
 	else :	
