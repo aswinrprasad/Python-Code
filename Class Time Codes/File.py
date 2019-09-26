89356a658562254b259fad63dedf5f5effccaeef
Aswin Raghuprasad <aswinrprasad@gmail.com>
2019-09-26 21:43:02 +0530

    RENAME of Class time codes to CODED.py

--- Class Time Codes/File.py
+++
@@ -1,6 +0,0 @@
-
-def file_read(fname):
-	txt=open(fname,"r")
-	print txt.read()
-
-file_read('read.txt')
--- Class Time Codes/TypeCasting.py
+++
@@ -1,6 +0,0 @@
-#Program to read an integer and a character representing an integer and to print their sum.
-
-x=int(input("Enter an integer number :"))
-ys=str(input("Enter a number as string :"))
-
-print "The sum of the two is :",x+int(ys)
