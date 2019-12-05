4db9c5fda6071f48b9901241c8013d08224946c4
Aswin Raghuprasad <aswinrprasad@gmail.com>
2019-12-05 18:24:10 +0530

    Memoization Implemented

--- CODED.py/editDistDP.py
+++ CODED.py/editDistDP.py
@@ -1,18 +1,26 @@
-def editDistance(str1, str2): 
-    if len(str1) == 0: 
-         return len(str2) 
+def editDistance(str1, str2, memo): 
+    if len(str1) == 0:
+        return len(str2)
 
     if len(str2) == 0: 
-        return len(str1)
+        memo[len(str1)][0]=len(str1)  
+        return memo[len(str1)][0] 
   
+    if memo[len(str1)-1][len(str2)-1] != None :
+        return memo[len(str1)-1][len(str2)-1]
+
     if str1[-1]== str2[-1]: 
-        return editDistance(str1[:-1], str2[:-1]) 
-  
-    return 1 + min(editDistance(str1, str2[:-1]),    # Insert 
-                   editDistance(str1[:-1], str2),    # Remove 
-                   editDistance(str1[:-1], str2[:-1])    # Replace 
-                   ) 
+        memo[len(str1)-1][len(str2)-1] = editDistance(str1[:-1], str2[:-1],memo) 
+        return memo[len(str1)-1][len(str2)-1]
+
+    memo[len(str1)-1][len(str2)-1] =  1 + min(editDistance(str1, str2[:-1] , memo),    # Insert 
+                editDistance(str1[:-1], str2, memo),    # Remove 
+                editDistance(str1[:-1], str2[:-1], memo)    # Replace 
+                )
+    return memo[len(str1)-1][len(str2)-1]  
+
   
 str1 = "ASWIN"
-str2 = "NIWSABaby"
-print editDistance(str1, str2) 
+str2 = "HelloWORLDWIN"
+memo=[[None for i in xrange(1000)] for k in xrange(len(str1)+1)]
+print editDistance(str1, str2 , memo) 
